9e351a4804d4ab545f7953d15a71a91a17423410
Aditya V Karve <adikarve01@gmail.com>
2019-03-14 19:46:57 +0530

    Update portScannerVanilla.py

--- portScannerVanilla.py
+++ portScannerVanilla.py
@@ -8,7 +8,7 @@ Created on Wed Mar 13 17:54:31 2019
 
 
 import socket
-
+#this script scans through the first 26 ports to check for open ports
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server='pythonprogramming.net'
 port=80
