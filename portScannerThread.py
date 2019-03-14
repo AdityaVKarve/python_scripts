05fbe8490ec2de17aab0476f5c02ff7c3f91f30c
Aditya V Karve <adikarve01@gmail.com>
2019-03-14 19:44:05 +0530

    Add files via upload

---
+++ portScannerThread.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 17:54:52 2019
+
+@author: thatguy99
+"""
+
+import socket
+import threading
+from Queue import Queue
+
+print_lock=threading.Lock()
+
+target='pythonprogramming.net'
+
+def portscan(port):
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    try:
+        con=s.connect((target,port))
+        with print_lock:
+            print('port ',port,' is open!')
+        con.close()
+    except:
+        pass
+def threader():
+    while True:
+        worker=q.get()
+        portscan(worker)
+        q.task_done()
+        
+q=Queue()
+for x in range(30):
+    t=threading.Thread(target=threader)
+    t.daemon=True
+    t.start()
+    
+for worker in range(1,65000):
+    q.put(worker)
+q.join()
---
+++ portScannerVanilla.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 17:54:31 2019
+
+@author: thatguy99
+"""
+
+
+import socket
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server='pythonprogramming.net'
+port=80
+def pscan(port):
+    try:
+        s.connect((server,port))
+        return True
+    except:
+        return False
+
+for x in range(1,26):
+        if pscan(x):
+            print('Port ',x,' is open')
+        else:
+            print('Port ',x,' is closed')
